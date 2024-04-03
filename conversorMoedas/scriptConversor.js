8143e4f9f630de8e7bf08e45e81b4d8353734535
airtonbjunior <airtonbjunior@gmail.com>
2024-04-03 09:21:35 -0300

    alteracoes aula 03abril

--- conversorMoedas/scriptConversor.js
+++ conversorMoedas/scriptConversor.js
@@ -1,15 +1,18 @@
 const valoresConversao = {
     real: {
         euro: 0.19,
-        dolar: 0.20
+        dolar: 0.20,
+        simbolo: "R$"
     },
     dolar: {
         real: 4.99,
-        euro: 0.92
+        euro: 0.92,
+        simbolo: "US$"
     },
     euro: {
         real: 5.40,
-        dolar: 1.08
+        dolar: 1.08,
+        simbolo: "EU"
     }
 }
 
@@ -17,6 +20,13 @@ const valoresConversao = {
 
 function converter() {
     let valorUsuario = document.getElementById("valorEntrada").value;
+
+    console.log(valorUsuario);
+
+    if(valorUsuario <= 0 || valorUsuario == "") {
+        alert("Verificar valor");
+        return;
+    }
     
     let moeda1 = document.getElementById("moeda1").value;
     let moeda2 = document.getElementById("moeda2").value;
@@ -26,10 +36,15 @@ function converter() {
         return;
     }
 
+    let simbolo = valoresConversao[moeda2]["simbolo"];
+    //console.log(simbolo);
+
     let resultado = valorUsuario * valoresConversao[moeda1][moeda2];
     
+    console.log(resultado);
+
     let paragrafoResultado = document.getElementById("resultado");
-    paragrafoResultado.textContent = resultado;
+    paragrafoResultado.textContent = simbolo + " " + resultado.toFixed(2);
 }
 
 function limpar() {
